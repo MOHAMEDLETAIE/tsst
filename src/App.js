6d000d2123921c09761e4ee1ab5f35c16ec6e0f7
MOHAMEDLETAIE <letaief768@gmail.com>
2024-07-15 16:21:29 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      import React from 'react';
+
+function App() {
+  <h1>hello</h1>
+} 
+
+export default App;
+
+    </div>
+  );
+}
+
+export default App;
